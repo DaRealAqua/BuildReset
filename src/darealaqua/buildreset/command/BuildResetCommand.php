c24a4d6e0278d21706683d6a1fe0a941d23a2a38
DaRealAqua <42686040+DaRealAqua@users.noreply.github.com>
2023-04-11 17:41:20 +0200

    Update BuildResetCommand.php

--- src/darealaqua/buildreset/command/BuildResetCommand.php
+++ src/darealaqua/buildreset/command/BuildResetCommand.php
@@ -10,9 +10,11 @@ namespace darealaqua\buildreset\command;
 use darealaqua\buildreset\Main;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned;
 use pocketmine\utils\TextFormat as C;
 
-class BuildResetCommand extends Command {
+class BuildResetCommand extends Command implements PluginOwned {
 
 	/**
 	 * @param Main $main
@@ -53,74 +55,81 @@ class BuildResetCommand extends Command {
 			return;
 		case "addworld":
 			if (!isset($args[1])) {
-				$sender->sendMessage(C::RED . "/buildreset addworld " . C::YELLOW . "<string:name>");
+				$sender->sendMessage(C::RED . "Usage: /buildreset addworld " . C::YELLOW . "<string:name>");
 				return;
 			}
-			if (in_array($args[1], $cfg->get("worlds"))) {
-				$sender->sendMessage(C::YELLOW . "World with the name '" . $args[1] . "' is already saved!");
+			if (in_array($args[1], $cfg->get("enabled_worlds"))) {
+				$sender->sendMessage(C::YELLOW . "The world with the name '" . $args[1] . "' is already saved.");
 				return;
 			}
 
-			$worlds = $cfg->get("worlds");
+			$worlds = $cfg->get("enabled_worlds");
 			$worlds[] = $args[1];
 
-			$cfg->set("worlds", $worlds);
+			$cfg->set("enabled_worlds", $worlds);
 			$cfg->save();
 
-			$sender->sendMessage(C::GRAY . "This world (" . C::YELLOW . $args[1] . C::GRAY . ") has been successfully saved.");
+			$sender->sendMessage(C::GRAY . "The world (" . C::YELLOW . $args[1] . C::GRAY . ") has been successfully saved. ");
 			break;
 		case "removeworld":
 			if (!isset($args[1])) {
-				$sender->sendMessage(C::RED . "/buildreset removeworld " . C::YELLOW . "<string:name>");
+				$sender->sendMessage(C::RED . "Usage: /buildreset removeworld " . C::YELLOW . "<string:name>");
 				return;
 			}
-			if (!in_array($args[1], $cfg->get("worlds"))) {
-				$sender->sendMessage(C::RED . "World with the name '" . $args[1] . "' doesn't exist!");
+			if (!in_array($args[1], $cfg->get("enabled_worlds"))) {
+				$sender->sendMessage(C::RED . "The world with the name '" . $args[1] . "' was not found.");
 				return;
 			}
 
-			$worlds = $cfg->get("worlds");
+			$worlds = $cfg->get("enabled_worlds");
 			$world = array_search($args[1], $worlds);
 			unset($worlds[$world]);
 
-			$cfg->set("worlds", array_values($worlds));
+			$cfg->set("enabled_worlds", array_values($worlds));
 			$cfg->save();
 
 			$sender->sendMessage(C::GRAY . "This world (" . C::RED . $args[1] . C::GRAY . ") has been successfully removed.");
 			break;
 		case "setdespawn":
 			if (!isset($args[1])) {
-				$sender->sendMessage(C::RED . "/buildreset setdespawn " . C::GREEN . "<int:seconds>");
+				$sender->sendMessage(C::RED . "Usage: /buildreset setdespawn " . C::GREEN . "<int:seconds>");
 				return;
 			}
 			if (!is_numeric($args[1])) {
-				$sender->sendMessage(C::RED . "Build reset despawn countdown must contain only numbers!");
+				$sender->sendMessage(C::RED . "The build reset despawn countdown can only contain numeric values.");
 				return;
 			}
 
 			$cfg->set("despawn_countdown", (int) $args[1]);
 			$cfg->save();
 
-			$sender->sendMessage(C::GRAY . "Build reset despawn countdown has been set to " . C::YELLOW . "'" . $args[1] . "'" . C::GRAY . " second(s).");
+			$sender->sendMessage(C::GRAY . "The build reset despawn countdown has been set to " . C::YELLOW . "'" . $args[1] . "'" . C::GRAY . " second(s).");
 			break;
 		case "setparticle":
 			if (!isset($args[1])) {
-				$sender->sendMessage(C::RED . "/buildreset setparticle " . C::GREEN . "<bool:true/false>");
+				$sender->sendMessage(C::RED . "Usage: /buildreset setparticle " . C::GREEN . "<bool:true/false>");
 				return;
 			}
 			$value = filter_var($args[1], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
 			if (!is_bool($value)) {
-				$sender->sendMessage(C::RED . "Build reset despawn particle must be bool e.g " . C::YELLOW . "<true|false>" . C::GRAY . ".");
+				$sender->sendMessage(C::RED . "The build reset despawn particle value must be a boolean (true or false).");
 				return;
 			}
 
 			$cfg->set("despawn_particle", $value);
 			$cfg->save();
 
-			$sender->sendMessage(C::GRAY . "BuildReset despawn particle has been set to " . C::YELLOW . "'" . $args[1] . "'" . C::GRAY . ".");
+			$sender->sendMessage(C::GRAY . "The build reset despawn particle has been set to " . C::YELLOW . "'" . $args[1] . "'" . C::GRAY . ".");
 			break;
 		default:
 			$sender->sendMessage(C::RED . $this->getUsage());
 		}
+	}
+
+	/**
+	 * @return Plugin
+	 */
+	public function getOwningPlugin(): Plugin {
+		return $this->main;
 	}
 }
